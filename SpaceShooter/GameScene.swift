627829c927300a2d10fb5d5265ddbd384801cfdb
Anatolii Afarinov <87117971+Aafarinov@users.noreply.github.com>
2021-07-25 21:29:17 +0300

    end of the first lesson

--- SpaceShooter/GameScene.swift
+++ SpaceShooter/GameScene.swift
@@ -1,13 +1,42 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var starfield:SKEmitterNode!
     var player: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Cчет: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
-       starfield = 
+       starfield = SKEmitterNode(fileNamed: "Starfield")
+        starfield.position = CGPoint(x: 0, y: 1472)
+        starfield.advanceSimulationTime(10)
+        self.addChild(starfield)
+        
+        starfield.zPosition = -1
+        
+        player = SKSpriteNode(imageNamed: "shuttle")
+        player.position = CGPoint(x: 0, y: -300)
+        
+        self.addChild(player)
+        
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        self.physicsWorld.contactDelegate = self
+    
+        scoreLabel = SKLabelNode(text: "Счет: 0")
+        scoreLabel.fontName = "AmericanTypewriter-Bold"
+        scoreLabel.fontSize = 56
+        scoreLabel.fontColor = UIColor.white
+        scoreLabel.position = CGPoint(x: -200, y: 500)
+        score = 0
+        
+        self.addChild(scoreLabel)
+
     }
     
     override func update(_ currentTime: TimeInterval) {
