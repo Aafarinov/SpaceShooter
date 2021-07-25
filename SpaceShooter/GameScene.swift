28b2eca10dce76e10c3f8cf27a7ccb4eb3bccd08
Anatolii Afarinov <87117971+Aafarinov@users.noreply.github.com>
2021-07-25 20:22:21 +0300

    first commit

---
+++ SpaceShooter/GameScene.swift
@@ -0,0 +1,16 @@
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var starfield:SKEmitterNode!
+    var player: SKSpriteNode!
+    
+    override func didMove(to view: SKView) {
+       starfield = 
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
